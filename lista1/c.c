20356ae74b60036c01be19e35013dce3a14dd71c
Fábio Henrique Mendes Brasileiro <fabio.h591@gmail.com>
2023-08-06 18:01:51 -0400

    treinando

---
+++ lista1/c.c
@@ -0,0 +1,6 @@
+//Criar um algoritmo que imprima a média aritmética dos números 5, 9 e 7. 
+#include <stdio.h>
+
+main() {
+    
+}
---
+++ test/2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+    char j = 0;
+    j += 1000;
+}
